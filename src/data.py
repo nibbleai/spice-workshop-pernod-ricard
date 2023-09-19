9e67d01167545282dd57fd1b32e11ef014a7b7c4
Lucas Le Ray <lucas.a.leray@gmail.com>
2023-09-19 10:56:24 +0200

    Add source data

--- src/data.py
+++ src/data.py
@@ -1,4 +1,3 @@
-import httpx
 import pandas as pd
 from sklearn.model_selection import train_test_split as _train_test_split
 
@@ -9,31 +8,19 @@ from src.schemas import TaxiColumn, WeatherColumn
 TAXI_DATA_FILENAME = 'nyc-taxi-2015.csv'
 WEATHER_DATA_FILENAME = 'nyc-weather-2015.csv'
 
-DATASET_URLS = {
-    TAXI_DATA_FILENAME: 'https://nibble-datasets.s3.eu-west-1.amazonaws.com/nyc-taxi/nyc-taxi-2015.csv',  # noqa: E501
-    WEATHER_DATA_FILENAME: 'https://nibble-datasets.s3.eu-west-1.amazonaws.com/nyc-taxi/nyc-weather-2015.csv'  # noqa: E501
-}
-
 
 def get_dataset():
-    filepath = directories.taxi_data_dir / TAXI_DATA_FILENAME
-    if not filepath.exists():
-        download_data(filepath)
-
     return pd.read_csv(
-        filepath,
+        directories.taxi_data_dir / TAXI_DATA_FILENAME,
         parse_dates=[TaxiColumn.PICKUP_TIME, TaxiColumn.DROPOFF_TIME]
     )
 
 
 def get_weather_data():
-    filepath = directories.weather_data_dir / WEATHER_DATA_FILENAME
-    if not filepath.exists():
-        download_data(filepath)
-
-    weather = pd.read_csv(filepath, parse_dates=[WeatherColumn.DATE])
-
-    return weather.assign(**{
+    return pd.read_csv(
+        directories.weather_data_dir / WEATHER_DATA_FILENAME,
+        parse_dates=[WeatherColumn.DATE]
+    ).assign(**{
         WeatherColumn.DATE: lambda x: x[WeatherColumn.DATE].dt.date
     })
 
@@ -52,10 +39,3 @@ def get_target(data):
         data[TaxiColumn.DROPOFF_TIME] - data[TaxiColumn.PICKUP_TIME]
     ).dt.seconds
     return target.rename(config.target)
-
-
-def download_data(filepath):
-    filepath.parent.mkdir(exist_ok=True, parents=True)
-    r = httpx.get(DATASET_URLS[filepath.name])
-    with open(filepath, 'w') as fp:
-        fp.write(r.text)
