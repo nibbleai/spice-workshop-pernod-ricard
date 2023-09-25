7e4dcea6ff26dbbc069cfcf28dff17cceed4751b
thomasf <thomas@nibble.ai>
2023-09-25 15:19:40 +0200

    Add pickup time feature

--- src/features.py
+++ src/features.py
@@ -3,5 +3,21 @@ from src.schemas import TaxiColumn
 
 registry = Registry("yannick")
 
-def pickup_hour(data):
-    return data[TaxiColumn.PICKUP_TIME].dt.hour
+# par défaut le nom de la feature sera le nom de la fonction
+@registry.register(
+    depends=["pickup_time"],
+) # enregistre la fonction dans le registry
+def pickup_hour(pickup_time):
+    return pickup_time.dt.hour
+
+
+@registry.register(
+    depends=["pickup_time"],
+)
+def pickup_year(pickup_time):
+    return pickup_time.dt.year
+
+
+@registry.register(name="pickup_time")
+def pickup_time(data):
+    return data[TaxiColumn.PICKUP_TIME]
