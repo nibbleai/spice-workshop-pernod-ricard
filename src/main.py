95e5ce6c232b4ff36b58775d2077fb8be8d7f210
Lucas Le Ray <lucas.a.leray@gmail.com>
2023-09-27 11:34:50 +0200

    Remove data split

--- src/main.py
+++ src/main.py
@@ -7,8 +7,6 @@ def main():
     target = get_target(data)
     data, target = preprocess(data, target)
 
-    train_data, test_data = train_test_split(data)
-
     train_features = ...  # TO IMPLEMENT
     test_features = ...  # TO IMPLEMENT
 
