3e53445507b396c569d23d442ba7002d64994eb0
Mathilde <mathilde.abolgassemi@pernod-ricard.com>
2023-09-28 08:47:28 +0000

    first registers

--- src/features.py
+++ src/features.py
@@ -1,3 +1,15 @@
 from spice import Registry
 
 registry = Registry('nyc-taxi')
+
+@registry.register(depends="pick_up_time")
+def pick_up_hour(pick_up_time):
+    return pick_up_time.dt.hour
+
+@registry.register(depends="pick_up_time")
+def pick_up_day(pick_up_time):
+    return pick_up_time.dt.day
+
+@registry.register()
+def pick_up_time(data):
+    return data.pickup_datetime
